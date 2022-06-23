d81d9c22e9970d324ee75b0dffe53c660ed96f6e
SiddhantChadha <sid.01chadha@gmail.com>
2022-06-23 15:16:52 +0530

    added cors

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const {getDataFromFlipkart, getDataFromAmazon,getDataFromCroma} = require('./scrape');
 
+const cors = require("cors");
+app.use(cors());
+
 app.get('/flipkart/:company/:product',async (req,res)=>{
     try{
         let data = await getDataFromFlipkart(req.params.product,req.params.company);
